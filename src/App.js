b6d31d94b3d77242a956a51d892297d55603dde3
kai-blt <blaitong@gmail.com>
2021-05-04 12:30:47 -0700

    adds form state and redux

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import {Search, Results, Nominations} from './components'
 function App() {
   return (
     <div>
-      <header class="pb-44">
+      <header className="pb-44">
         <Search />
       </header>
-      <section class="px-4">
+      <section className="px-4">
         <Results />
         <Nominations />
       </section>
